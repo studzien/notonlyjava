1b2f6be696e3a7a720bf5e8d03dcadcea006a2e9
Rafał Studnicki <rafal@opencubeware.org>
2013-11-17 23:45:26 +0100

    Add NDK examples

---
+++ hello-jni/obj/local/armeabi/objs-debug/hello-jni/hello-jni.o.d
@@ -0,0 +1,19 @@
+obj/local/armeabi/objs-debug/hello-jni/hello-jni.o: jni/hello-jni.c \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/string.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/malloc.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/jni.h
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/string.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/jni.h:
---
+++ native-activity/obj/local/armeabi/objs/native-activity/main.o.d
@@ -0,0 +1,181 @@
+obj/local/armeabi/objs/native-activity/main.o: jni/main.c \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/jni.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/errno.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/linux/errno.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm/errno.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/EGL/egl.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/EGL/eglplatform.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/KHR/khrplatform.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/stdint.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/native_window.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/rect.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/GLES/gl.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/GLES/glplatform.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/sensor.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/looper.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/log.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/sources/android/native_app_glue/android_native_app_glue.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/poll.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/linux/poll.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm/poll.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/pthread.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/time.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/time.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/linux/time.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm/siginfo.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/signal.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/limits.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/syslimits.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm/page.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/string.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/malloc.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm/signal.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sched.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/configuration.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/asset_manager.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/native_activity.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/input.h \
+ /Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/keycodes.h
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/jni.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/errno.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/linux/errno.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm/errno.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/EGL/egl.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/EGL/eglplatform.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/KHR/khrplatform.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/native_window.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/rect.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/GLES/gl.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/GLES/glplatform.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/sensor.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/looper.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/log.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/sources/android/native_app_glue/android_native_app_glue.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/poll.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/linux/poll.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm/poll.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/pthread.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/time.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/time.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/linux/time.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm/siginfo.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/signal.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/limits.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm/page.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/string.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm/signal.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/sched.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/configuration.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/asset_manager.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/native_activity.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/input.h:
+
+/Users/Studnicki/work/adt/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/keycodes.h:
